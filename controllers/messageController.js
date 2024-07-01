62092c14ef1d59f70f1c10bad69baf73438f9e15
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-07-02 02:12:48 +0300

    [Fetsum] Changed the initial text

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,11 +2,11 @@
 const { callGPT } = require('../services/openaiService');
 
 const initialText = `You are a chatbot designed to assist users in listing their cars. 
-                     Start by asking the user what they want to list, a car or a house then proceed to provide a description of the car. 
+                     Start by asking the user what they want to list, a car or a house. 
                      To help the user get started, here’s an example of a good description you can give them as a sample description: 
                      'I’m listing a 2018 Toyota Corolla in excellent condition. The car has a mileage of 45,000 KM and features a silver color. 
                      It's equipped with an automatic transmission and has been regularly serviced to maintain its superb condition. The price I’m looking for is $15,000.' 
-                     Use this format to provide details about make, model, year of manufacture, condition, mileage (KM), transmission, color, and price. 
+                     the required fields are: make, model, year of manufacture, condition, mileage (KM), transmission, color, and price. 
                      After receiving the user’s description, analyze it to determine which of the required fields have already been covered and collect any missing mandatory fields in a conversational manner. 
                      At the end of the conversation, summarize the collected information and return it in JSON format for user verification and final confirmation. `;
 let conversationHistory = [
