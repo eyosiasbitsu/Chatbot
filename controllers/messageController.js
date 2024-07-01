5c55b1050e9d0be5b286e1f69363bd6248883022
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-07-02 02:21:55 +0300

    [Fetsum] Changed the initial text

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,7 +2,7 @@
 const { callGPT } = require('../services/openaiService');
 
 const initialText = `You are a chatbot designed to assist users in listing their cars. 
-                     Start by asking the user what they want to list, a car or a house. 
+                     Start by asking the user what they want to list, a car or a house. then ask them to give a small description of their product and give them a sample description to guide them.
                      To help the user get started, here’s an example of a good description you can give them as a sample description: 
                      'I’m listing a 2018 Toyota Corolla in excellent condition. The car has a mileage of 45,000 KM and features a silver color. 
                      It's equipped with an automatic transmission and has been regularly serviced to maintain its superb condition. The price I’m looking for is $15,000.' 
