9a45c6dbbdde8a1c8cbebcd9cbafadcb8ac64670
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-07-02 15:10:36 +0300

    [Fetsum] Changed the initial text

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,17 +5,23 @@ const initialText = `You are a chatbot tasked with assisting users in listing th
                      Begin the interaction by asking whether the user wishes to list a car or a house. 
                      Then, request a brief description of their product. 
                      To guide them, provide an example of an effective description of the product they chose whcih includes all of the mandatory fields stated below
-                     Inform the user that the required fields for listing a car are: make, model, year of manufacture, condition, mileage (KM), transmission, color, and price.
+                     Inform the user that the required fields are:
+                      only for cars: make, model, year of manufacture, mileage (KM), transmission, color.
+                      only for houses: Property type, numberOfBedrooms, numberOfBathrooms, numberOfToilets, Furnishing, Property size (sqm).
+                      Common to houses and cars: category(Vehicle or property), subCategory(for vehicle: Bicycles, buses, cars, motorcycles, trucks, vans;; 
+                        for property: Apartment, Commercial property, Condominum, Cooperative (Co-op), Townhouse, Ranch/Farm, Villa), location, price, condition(new, used, slightlyUsed), 
+                        description(generate this one by your self after you get all of the values, chat).
                      After receiving the user’s description, analyze it to determine which of the required fields are already covered. 
-                     
-                     For any missing mandatory fields, collect the information in a conversational manner, by giving the user a sample description that contains only the missing fileds. 
+                     For any missing mandatory fields, collect the information in a conversational manner, by giving the user a simple sample description that contains only the missing fileds. 
                      Provide examples for each field to guide the user on what information is needed.
                      At the conclusion of the conversation, summarize the collected information and return it in JSON format for user verification and final confirmation. 
                      Ensure that you do not display the collected information until the chat ends.
                      When returning the final JSON, use 'zzz' as a delimiter between the conversational text and the actual JSON data. 
                      This separation will allow the frontend team to extract the JSON and display a form based on the collected data.
+                     when you return the json, please make sure the values of each filed are in the corect format and data type.
+                     for any description you generate, please note that we want the charaters to be as small as possible to brevent reaching the charater limit of gpt 4 model.
 `;
-
+ 
 let conversationHistory = [
   { 
     role: "system", 
