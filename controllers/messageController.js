b50d0def22225a71004bd5bd0d8da8449a375421
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-07-02 15:57:28 +0300

    [Fetsum] Changed the initial text

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,7 +5,7 @@ const initialText = `You are a chatbot tasked with assisting users in listing th
                      Begin the interaction by asking whether the user wishes to list a car or a house. 
                      Then, request a brief description of their product. 
                      To guide them, provide an example of an effective description of the product they chose whcih includes all of the mandatory fields stated below
-                     chat, please note that the following are required fileds and please don't mention them in the description since it will make the description long(I want it simple and yeasy):
+                     chat, please note that the following are required fileds and please don't ask them to mention them in the description since it will make the description long(I want it simple and yeasy):
                       only for cars: make, model, year of manufacture, mileage (KM), transmission, color.
                       only for houses: Property type, numberOfBedrooms, numberOfBathrooms, numberOfToilets, Furnishing, Property size (sqm).
                       Common to houses and cars: category(Vehicle or property), subCategory(for vehicle: Bicycles, buses, cars, motorcycles, trucks, vans;; 
@@ -14,7 +14,7 @@ const initialText = `You are a chatbot tasked with assisting users in listing th
                      After receiving the user’s description, analyze it to determine which of the required fields are already covered. 
                      For any missing mandatory fields, collect the information in a conversational manner, by giving the user a simple sample description that contains only the missing fileds. 
                      Provide examples for each field to guide the user on what information is needed.
-                     At the conclusion of the conversation, summarize the collected information and return it in JSON format for user verification and final confirmation. 
+                     At the conclusion of the conversation, summarsize the collected information and return it in JSON format for user verification and final confirmation. 
                      Ensure that you do not display the collected information until the chat ends.
                      When returning the final JSON, use 'zzz' as a delimiter between the conversational text and the actual JSON data. 
                      This separation will allow the frontend team to extract the JSON and display a form based on the collected data.
