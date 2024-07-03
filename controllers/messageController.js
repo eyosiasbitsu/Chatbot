dd47df8ea43c2c0e6772496bdb377f6556943467
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-07-03 14:32:42 +0300

    [Fetsum] Changed the initial text

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,7 +4,8 @@ const { callGPT } = require('../services/openaiService');
 const initialText = `You are a chatbot tasked with assisting users in listing their cars for sale. 
                      Begin the interaction by asking whether the user wishes to list a car or a house. 
                      Then, request a brief description of their product. 
-                     To guide them, provide an example of an effective description of the product they chose whcih includes all of the mandatory fields stated below
+                     To guide them, provide an example of an effective description of the product they chose whcih includes all of the mandatory fields stated below.
+                     for any sample description you generate, please note that we want the charaters to be as small as possible to brevent reaching the charater limit of gpt 4 model.
                      chat, please note that the following are required fileds and please don't ask them to mention them in the description since it will make the description long(I want it simple and yeasy):
                       only for cars: make, model, year of manufacture, mileage (KM), transmission, color.
                       only for houses: Property type, numberOfBedrooms, numberOfBathrooms, numberOfToilets, Furnishing, Property size (sqm).
@@ -16,10 +17,10 @@ const initialText = `You are a chatbot tasked with assisting users in listing th
                      Provide examples for each field to guide the user on what information is needed.
                      At the conclusion of the conversation, summarsize the collected information and return it in JSON format for user verification and final confirmation. 
                      Ensure that you do not display the collected information until the chat ends.
-                     When returning the final JSON, use 'zzz' as a delimiter between the conversational text and the actual JSON data. 
+                     When returning the final JSON, return it like this: Thank you     ZZZ  JSON.
+                     We can use the ZZZ as a delimeter to know the rest of the string is the JSON file.
                      This separation will allow the frontend team to extract the JSON and display a form based on the collected data.
                      when you return the json, please make sure the values of each filed are in the corect format and data type.
-                     for any description you generate, please note that we want the charaters to be as small as possible to brevent reaching the charater limit of gpt 4 model.
 `;
 
 let conversationHistory = [
